01d1f5aad919433ef2b4bb36734949007ce06dfe
Xiaoyao Li <xiaoyao.li@intel.com>
2020-06-11 16:31:37 +0800

    *** HACK *** linux-headers: Update headers to pull in TDX API changes
    Pull in recent TDX updates, which are not backwards compatible.
    
    It's just to make this series runnable. It will be updated by script
    
    	scripts/update-linux-headers.sh
    
    once TDX support is upstreamed in linux kernel.
    
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Co-developed-by: Sean Christopherson <sean.j.christopherson@intel.com>
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>

--- linux-headers/linux/kvm.h
+++ linux-headers/linux/kvm.h
@@ -1134,6 +1134,8 @@ struct kvm_ppc_resize_hpt {
 #define KVM_CAP_VM_GPA_BITS 207
 #define KVM_CAP_XSAVE2 208
 
+#define KVM_CAP_VM_TYPES 1000
+
 #ifdef KVM_CAP_IRQ_ROUTING
 
 struct kvm_irq_routing_irqchip {
