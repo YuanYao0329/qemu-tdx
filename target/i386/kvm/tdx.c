bc4b2f278af08ef667b4adc9143277257b6f842a
Xiaoyao Li <xiaoyao.li@intel.com>
2021-12-08 13:46:25 +0800

    i386/tdx: Get tdx_capabilities via KVM_TDX_CAPABILITIES
    KVM provides TDX capabilities via sub command KVM_TDX_CAPABILITIES of
    IOCTL(KVM_MEMORY_ENCRYPT_OP). Get the capabilities when initializing
    TDX context. It will be used to validate user's setting later.
    
    Besides, introduce the interfaces to invoke TDX "ioctls" at different
    scope (platform, VM and VCPU) in preparation.
    
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>

--- target/i386/kvm/tdx.c
+++ target/i386/kvm/tdx.c
@@ -13,11 +13,85 @@
 
 #include "qemu/osdep.h"
 #include "qom/object_interfaces.h"
+#include "sysemu/kvm.h"
 
 #include "hw/i386/x86.h"
 #include "tdx.h"
 
 #define TDX_TD_ATTRIBUTES_DEBUG     BIT_ULL(0)
+
+enum tdx_ioctl_level{
+    TDX_PLATFORM_IOCTL,
+    TDX_VM_IOCTL,
+    TDX_VCPU_IOCTL,
+};
+
+static int __tdx_ioctl(void *state, enum tdx_ioctl_level level, int cmd_id,
+                        __u32 metadata, void *data)
+{
+    struct kvm_tdx_cmd tdx_cmd;
+    int r;
+
+    memset(&tdx_cmd, 0x0, sizeof(tdx_cmd));
+
+    tdx_cmd.id = cmd_id;
+    tdx_cmd.metadata = metadata;
+    tdx_cmd.data = (__u64)(unsigned long)data;
+
+    switch (level) {
+    case TDX_PLATFORM_IOCTL:
+        r = kvm_ioctl(kvm_state, KVM_MEMORY_ENCRYPT_OP, &tdx_cmd);
+        break;
+    case TDX_VM_IOCTL:
+        r = kvm_vm_ioctl(kvm_state, KVM_MEMORY_ENCRYPT_OP, &tdx_cmd);
+        break;
+    case TDX_VCPU_IOCTL:
+        r = kvm_vcpu_ioctl(state, KVM_MEMORY_ENCRYPT_OP, &tdx_cmd);
+        break;
+    default:
+        error_report("Invalid tdx_ioctl_level %d", level);
+        exit(1);
+    }
+
+    return r;
+}
+
+#define tdx_platform_ioctl(cmd_id, metadata, data) \
+        __tdx_ioctl(NULL, TDX_PLATFORM_IOCTL, cmd_id, metadata, data)
+
+#define tdx_vm_ioctl(cmd_id, metadata, data) \
+        __tdx_ioctl(NULL, TDX_VM_IOCTL, cmd_id, metadata, data)
+
+#define tdx_vcpu_ioctl(cpu, cmd_id, metadata, data) \
+        __tdx_ioctl(cpu, TDX_VCPU_IOCTL, cmd_id, metadata, data)
+
+static struct kvm_tdx_capabilities *tdx_caps;
+
+static void get_tdx_capabilities(void)
+{
+    struct kvm_tdx_capabilities *caps;
+    int max_ent = 1;
+    int r, size;
+
+    do {
+        size = sizeof(struct kvm_tdx_capabilities) +
+               max_ent * sizeof(struct kvm_tdx_cpuid_config);
+        caps = g_malloc0(size);
+        caps->nr_cpuid_configs = max_ent;
+
+        r = tdx_platform_ioctl(KVM_TDX_CAPABILITIES, 0, caps);
+        if (r == -E2BIG) {
+            g_free(caps);
+            max_ent *= 2;
+        } else if (r < 0) {
+            error_report("KVM_TDX_CAPABILITIES failed: %s\n", strerror(-r));
+            exit(1);
+        }
+    }
+    while (r == -E2BIG);
+
+    tdx_caps = caps;
+}
 
 int tdx_kvm_init(MachineState *ms, Error **errp)
 {
@@ -25,6 +99,10 @@ int tdx_kvm_init(MachineState *ms, Error **errp)
                                                     TYPE_TDX_GUEST);
     if (!tdx) {
         return -EINVAL;
+    }
+
+    if (!tdx_caps) {
+        get_tdx_capabilities();
     }
 
     return 0;
