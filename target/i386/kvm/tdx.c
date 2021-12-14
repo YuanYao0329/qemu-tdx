095be2805b80c045fe07851e2a15ed7075bc4709
Xiaoyao Li <xiaoyao.li@intel.com>
2021-12-14 10:36:11 +0800

    i386/tdx: Finalize TDX VM
    Invoke KVM_TDX_FINALIZE_VM to finalize the TD's measurement and make
    the TD vCPUs runnable once machine initialization is complete.
    
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>

--- target/i386/kvm/tdx.c
+++ target/i386/kvm/tdx.c
@@ -291,6 +291,13 @@ static void tdx_finalize_vm(Notifier *notifier, void *unused)
              exit(0);
         }
     }
+
+    r = tdx_vm_ioctl(KVM_TDX_FINALIZE_VM, 0, NULL);
+    if (r < 0) {
+        error_report("KVM_TDX_FINALIZE_VM failed %s", strerror(-r));
+        exit(0);
+    }
+    tdx_guest->parent_obj.ready = true;
 }
 
 static Notifier tdx_machine_done_late_notify = {
